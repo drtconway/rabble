26d9f050cd5893a19f5ad47c566f2e09fcd6e4eb
Thomas Conway <thomas.conway@petermac.org>
2019-05-22 11:17:56 +1000

    Start hacking to use more specific directives

---
+++ examples/media/report.js
@@ -0,0 +1,50 @@
+
+rangy.init();
+
+var CiterButton = MediumEditor.extensions.button.extend({
+    name: 'citer',
+    tagNames: ['cite'],
+    contentDefault: '<b>[]</b>',
+    contentFA: '<i class="fa fa-book"></i>',
+    aria: 'Cite',
+    action: 'cite',
+
+    init: function () {
+        MediumEditor.extensions.button.prototype.init.call(this);
+
+        this.classApplier = rangy.createClassApplier('cite', {
+            elementTagName: 'cite',
+            normalize: true
+        });
+    },
+
+    handleClick: function (event) {
+        this.classApplier.toggleSelection();
+
+        // Ensure the editor knows about an html change so watchers are notified
+        // ie: <textarea> elements depend on the editableInput event to stay synchronized
+        this.base.checkContentChanged();
+    }
+});
+
+document.querySelectorAll('[data-rabble-rich][data-rabble-edit="true"]').forEach((e) => {
+    e.setAttribute('contenteditable', 'true');
+    e.classList.add("editable");
+    new MediumEditor(e, {
+        toolbar: {
+            static: true,
+            sticky: true,
+            buttons: ['bold', 'italic', 'underline']
+        }
+    });
+});
+document.querySelectorAll('[data-rabble-text][data-rabble-edit="true"').forEach((e) => {
+    e.setAttribute('contenteditable', 'true');
+    e.classList.add("editable");
+    new MediumEditor(e, {
+      disableReturn: true,
+      placeholder: {
+        text: 'type here'
+      }
+    });
+});

3159c5aa849da4d54ca1d959128717f3e02e58ff
Thomas Conway <drtomc@gmail.com>
2019-05-27 03:51:19 +0000

    More testing.

--- src/test/java/org/petermac/rabble/RabbleTest.java
+++ src/test/java/org/petermac/rabble/RabbleTest.java
@@ -18,7 +18,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonString;
 import javax.json.JsonValue;
 
 import org.w3c.dom.Document;
@@ -29,6 +31,87 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
 class RabbleTest {
+    public static final void assertXmlEquals(Node lhs, Node rhs) {
+        assertEquals(lhs.getNodeType(), rhs.getNodeType());
+        switch (lhs.getNodeType()) {
+            case Node.ELEMENT_NODE:
+                Element lhsElem = (Element) lhs;
+                Element rhsElem = (Element) rhs;
+                assertEquals(lhs.getNodeName(), rhs.getNodeName());
+
+                /* Check attributes */
+                NamedNodeMap lhsAttrs = lhsElem.getAttributes();
+                NamedNodeMap rhsAttrs = rhsElem.getAttributes();
+                for (int i = 0; i < lhsAttrs.getLength(); i++) {
+                    /* for each lhs attr, check the rhs one is there. */
+                    Node lhsAttr = lhsAttrs.item(i);
+                    assertTrue(rhsElem.hasAttribute(lhsAttr.getNodeName()));
+                    Node rhsAttr = rhsAttrs.getNamedItem(lhsAttr.getNodeName());
+                    assertEquals(lhsAttr.getNodeName(), rhsAttr.getNodeName());
+                    assertEquals(lhsAttr.getNodeValue(), rhsAttr.getNodeValue());
+                }
+                for (int i = 0; i < rhsAttrs.getLength(); i++) {
+                    /* for each rhs attr, check the lhs one is there. */
+                    Node rhsAttr = rhsAttrs.item(i);
+                    assertTrue(lhsElem.hasAttribute(rhsAttr.getNodeName()));
+                    Node lhsAttr = lhsAttrs.getNamedItem(rhsAttr.getNodeName());
+                    assertEquals(lhsAttr.getNodeName(), rhsAttr.getNodeName());
+                    assertEquals(lhsAttr.getNodeValue(), rhsAttr.getNodeValue());
+                }
+
+                /* Check children */
+                NodeList lhsKids = lhsElem.getChildNodes();
+                NodeList rhsKids = rhsElem.getChildNodes();
+                assertEquals(lhsKids.getLength(), rhsKids.getLength());
+                for (int i = 0; i < lhsKids.getLength(); i++) {
+                    assertXmlEquals(lhsKids.item(i), rhsKids.item(i));
+                }
+
+                break;
+
+            case Node.TEXT_NODE:
+                assertEquals(lhs.getNodeValue(), rhs.getNodeValue());
+                break;
+
+            default:
+                /* ignore comments, etc. */
+                break;
+        }
+    }
+
+    public static final void assertJsonEquals(JsonValue lhs, JsonValue rhs) {
+        assertEquals(lhs.getValueType(), rhs.getValueType());
+        switch (lhs.getValueType()) {
+            case STRING:
+                JsonString lhsTxt = (JsonString)lhs;
+                JsonString rhsTxt = (JsonString)rhs;
+                assertEquals(lhsTxt.getString(), rhsTxt.getString());
+                break;
+
+            case ARRAY:
+                JsonArray lhsArray = (JsonArray)lhs;
+                JsonArray rhsArray = (JsonArray)rhs;
+                assertEquals(lhsArray.size(), rhsArray.size());
+                for (int i = 0; i < lhsArray.size(); i++) {
+                    assertJsonEquals(lhsArray.get(i), rhsArray.get(i));
+                }
+                break;
+
+            case OBJECT:
+                JsonObject lhsObject = (JsonObject)lhs;
+                JsonObject rhsObject = (JsonObject)rhs;
+                assertEquals(lhsObject.size(), rhsObject.size());
+                Set<String> lhsKeys = lhsObject.keySet();
+                Set<String> rhsKeys = rhsObject.keySet();
+                assertEquals(lhsKeys, rhsKeys);
+                for (String key : lhsKeys) {
+                    JsonValue lhsKid = lhsObject.get(key);
+                    JsonValue rhsKid = rhsObject.get(key);
+                    assertJsonEquals(lhsKid, rhsKid);
+                }
+        }
+    }
+
     @Test
     public void testEmptyTemplate() throws Exception {
         String docText = Utils.lines(
@@ -88,19 +171,52 @@ class RabbleTest {
         assertNotNull(rabble);
 
         Element e = rabble.instantiate(data);
-        String eStr = Utils.serialize(e);
 
         String resText = Utils.lines(
             "<html>",
             " <head>",
             " </head>",
             " <body>",
-            "  <div data-rabble-kind=\"group\" data-rabble-name=\"request\">",
-            "   <div data-rabble-kind=\"text\" data-rabble-name=\"urn\">PMEX1234567</div>",
+            "  <div data-rabble-kind='group' data-rabble-name='request'>",
+            "   <div data-rabble-kind='text' data-rabble-name='urn'>PMEX1234567</div>",
             "  </div>",
             " </body>",
             "</html>"
         );
-        assertEquals(resText, eStr);
+        Document resDoc = Utils.textToDoc(resText);
+        assertXmlEquals(resDoc.getDocumentElement(), e);
+    }
+
+    @Test
+    public void testSimpleExtract() throws Exception {
+        String docText = Utils.lines(
+            "<html>",
+            " <head>",
+            " </head>",
+            " <body>",
+            "  <div data-rabble-name='request' data-rabble-kind='group'>",
+            "   <div data-rabble-name='urn' data-rabble-kind='text'>PMEX1234567</div>",
+            "  </div>",
+            " </body>",
+            "</html>"
+        );
+        Document doc = Utils.textToDoc(docText);
+
+        assertNotNull(doc);
+
+        String jsonText = Utils.lines(
+            "{\"request\": {\"urn\": \"PMEX1234567\"}}"
+        );
+        JsonValue json = Utils.textToJson(jsonText);
+        assertEquals(JsonValue.ValueType.OBJECT, json.getValueType());
+        JsonObject data = (JsonObject)json;
+        assertEquals(1, data.size());
+
+        Rabble rabble = new Rabble(doc, doc.getDocumentElement());
+        assertNotNull(rabble);
+
+        JsonValue e = rabble.extract(doc.getDocumentElement());
+        assertNotNull(e);
+        assertJsonEquals(json, e);
     }
 }

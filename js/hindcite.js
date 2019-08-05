c5568b475ec1cd22d1e0b7e6311fe4dd25155305
Thomas Conway <thomas.conway@petermac.org>
2019-08-05 16:21:46 +1000

    More hindcite work.

--- js/hindcite.js
+++ js/hindcite.js
@@ -82,10 +82,141 @@ class PubmedScraper {
     }
 }
 
+/**
+ * Class for managing citations and references in an HTML document.
+ *
+ * Limitations:
+ *  - Hindcite only supports 1 citation style: numbered citations, ordered by position of first appearence.
+ *  - All references are identified by a PMID. If it doesn't appear on Pubmed, we can't do it! But then, if
+ *    it's not on Pubmed, it's not real science. :D
+ *
+ * The document must contain an element with the id 'hindcite-references' in which the list of cited references is to be placed.
+ * The typical structure is to have a heading element followed by the div container:
+ *     ...
+ *     <h1>References</h1>
+ *     <div id='hindcite-references'>
+ *     </div>
+ *     ...
+ *
+ * A reference has a fixed structures:
+ *     <div data-hindcite-ref id='PMID12345'><span>7.</span>...content...</div>
+ *
+ * The id attribute gives the Pubmed ID, and the text content of the span gives the label.
+ * CSS Styling for the span can be achieved using a selector such as:
+ *
+ *  div[data-hindcite-ref] span:first-child {
+ *      padding-right: 1em;
+ *  }
+ *
+ * Citation groups all occur within a span tagged with a 'data-hindcite-cite' attribute:
+ *  <span data-hindcite-cite>...</span>
+ *
+ * Individual citations themselves are formatted as anchor tags:
+ *  <a data-hindcite-key='PMID12345' href='#PMID12345'>7</a>
+ *
+ * Citation groups (which may be singleton) can be reformatted with the function reformatCitation,
+ * which recreates the content of the citation group span element with the following format:
+ *  <span data-hindcite-cite>[<a ...>n_1</a>, <a ...>n_2</a>, ...]</span>
+ *
+ * Additionally, the function parseCitationNode can be used to scan a citation group node for new
+ * citations. To do this, the text node children of the citation group are scanned for pubmed ids
+ * with the regular expression /PMID:([0-9]+)/, and any matches are replaced with an anchor node
+ * with the correct format.
+ *
+ * To recompile the references, the method recomputeReferences is used. It scans the document looking
+ * for citations, and compiles the list of citations in order. It then looks to see which ones are in the
+ * document, and if any are missing or unused. If a reference resolver has been supplied, it attempts to
+ * look up and missing references. The references div is then repopulated with the references in first-citation
+ * order, and any unused ones are added at the end.
+ *
+ * Unused references have a 'data-hindcite-unused' attribute added, so that unused references may be easily identified,
+ * and hidden if desired. To hide unused references, CSS such as the following could be used:
+ *  div[data-hindcite-unused] {
+ *      display: none;
+ *  }
+ */
 class Hindcite {
     constructor(doc) {
         this.doc = doc;
         this.refs = doc.getElementById('hindcite-references');
+    }
+
+    /**
+     * Parse a node (usually a span tag) with a 'data-hindcite-cite' tag.
+     */
+    parseCitationNode(citeNode) {
+        if (!citeNode.hasAttribute('data-hindcite-cite') {
+            // not a citation node!
+            return;
+        }
+
+        let nodes = [];
+        for (let i = 0; i < citeNode.childNodes.length; i++) {
+            let node = citeNode.childNodes[i];
+            if (node.nodeType != Node.TEXT_NODE)  {
+                nodes.push(node);
+                continue;
+            }
+            let parts = node.textContent.split(/PMID:([0-9]+)/);
+            if (parts.length == 1) {
+                nodes.push(node);
+                continue;
+            }
+            for (let j = 0; j < parts.length; j++) {
+                if ((j & 1) == 0) {
+                    // Parts 0, 2, ... are text
+                    if (parts[j].length > 0) {
+                        citeNode.appendChild(this.doc.createTextNode(parts[j]));
+                    }
+                } else {
+                    // Parts 1, 3, ... are PMIDs
+                    let a = this.doc.createElement('a');
+                    let k = 'PMID' + parts[j];
+                    a.setAttribute('data-hindcite-key', k);
+                    a.setAttribute('href', '#' + k);
+                    a.textContent = '?';
+                    nodes.push(a);
+                }
+            }
+        }
+        this.removeAllChildren(citeNode);
+        for (let i = 0; i < nodes.length; i++) {
+            citeNode.appendChild(nodes[i]);
+        }
+    }
+
+    reformatCitation(citeNode) {
+        if (!citeNode.hasAttribute('data-hindcite-cite') {
+            // not a citation node!
+            return;
+        }
+        let seen = {};
+        let nodes = [];
+        for (let i = 0; i < citeNode.childNodes.length; i++) {
+            let node = citeNode.childNodes[i];
+            if (node.nodeType != Node.ELEMENT_NODE)  {
+                continue;
+            }
+            if (!node.hasAttribute('data-hindcite-key')) {
+                continue;
+            }
+            let k = node.getAttribute('data-hindcite-key');
+            if (seen[k] != null) {
+                continue;
+            }
+            nodes.push(node);
+            seen[k] = node;
+        }
+
+        this.removeAllChildren(citeNode);
+        citeNode.appendChild(this.doc.createTextNode('['));
+        for (let i = 0; i < nodes.length; i++) {
+            if (i > 0) {
+                citeNode.appendChild(this.doc.createTextNode(', '));
+            }
+            citeNode.appendChild(nodes[i]);
+        }
+        citeNode.appendChild(this.doc.createTextNode(']'));
     }
 
     recomputeReferences() {
@@ -138,13 +269,30 @@ class Hindcite {
 
         // Now remove all the references, and re-attach the ones that have citations.
         //
-        while (this.refs.hasChildren()) {
-            this.refs.removeChild(this.refs.childNodes[0]);
-        }
+        this.removeAllChildren(this.refs);
         for (let i = 0; i < pmids.length; i++) {
             let pmid = pmids[i];
             let node = jdx[pmid];
+            if (node.hasAttribute('data-hindcite-unused')) {
+                node.removeAttribute('data-hindcite-unused');
+            }
             this.refs.appendChild(node);
+            this.setCitationLabel(i+1 + '.', node);
+            delete jdx[pmid];
+        }
+
+        // Now reattach all the extra ones and add a 'data-hindcite-unused' attribute
+        // so that CSS can be used to hide them if desired.
+        //
+        let extras = Object.keys(jdx).sort();
+        for (let i = 0; i < extras.length; i++) {
+            let pmid = extras[i];
+            let node = jdx[pmid];
+            if (!node.hasAttribute('data-hindcite-unused')) {
+                node.setAttribute('data-hindcite-unused', null);
+            }
+            this.refs.appendChild(node);
+            this.setCitationLabel('?.', node);
         }
     }
 
@@ -182,6 +330,21 @@ class Hindcite {
             }
         }
         return refNodes;
+    }
+
+    setCitationLabel(lab, node) {
+        if (node.nodeName == 'div' && node.hasAttribute('data-hindcite-ref')) {
+            let kid0 = node.firstChild;
+            if (kid0.nodeName == 'span') {
+                kid0.textContent = lab;
+            }
+        }
+    }
+
+    removeAllChildren(node) {
+        while (node.hasChildren()) {
+            node.removeChild(node.childNodes[0]);
+        }
     }
 
 }
